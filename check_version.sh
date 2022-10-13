d3a57c9939fb12f113af5791cb02788a90bf4779
canozokur <13416785+canozokur@users.noreply.github.com>
2022-10-13 12:42:52 +0300

    Add sha512sum to notifications

--- check_version.sh
+++ check_version.sh
@@ -7,11 +7,14 @@ send_dm() {
 
 CURRENT_AUR_VERSION=$(curl -s 'https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=beyond-identity-bin' | jq .results[].Version -r | cut -d'-' -f1 | sed 's/_/-/g')
 
-LATEST_BUILD_VERSION=$(curl -s -G --data-urlencode 'query=format:raw' 'https://api.cloudsmith.io/v1/packages/beyond-identity/linux-authenticator/' | jq '.|max_by(.version)|.version' -r)
+CLOUDSMITH_RESPONSE=$(curl -s -G --data-urlencode 'query=format:raw' 'https://api.cloudsmith.io/v1/packages/beyond-identity/linux-authenticator/')
+LATEST_BUILD_VERSION=$(echo "${CLOUDSMITH_RESPONSE}" | jq '.|max_by(.version)|.version' -r)
+LATEST_BUILD_HASH=$(echo "${CLOUDSMITH_RESPONSE}" | jq '.|max_by(.version)|.files[].checksum_sha512' -r)
 
 if [ "${LATEST_BUILD_VERSION}" != "${CURRENT_AUR_VERSION}" ]; then
 	send_dm "Heads up! A different beyond-identity version is available: ${LATEST_BUILD_VERSION}"
 	send_dm "Current version is ${CURRENT_AUR_VERSION}"
+	send_dm "Latest version's hash is: ${LATEST_BUILD_HASH}"
 	send_dm "Check https://cloudsmith.io/~beyond-identity/repos/linux-authenticator/packages/?q=filename%3Agz%24 for all packages."
 	exit 0
 fi
